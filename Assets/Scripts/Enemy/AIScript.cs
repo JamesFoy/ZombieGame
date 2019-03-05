0b3c5b3d9445c405ea62efaf6744a0f7e3060540
Jfoy <39011272+JamesFoy@users.noreply.github.com>
2019-03-05 12:58:31 +0000

    Added AI States

---
+++ Assets/Scripts/Enemy/AIScript.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIScript : MonoBehaviour {
+
+    private Animator anim;
+    [SerializeField]
+    private bool isDead;
+    private bool isMoving;
+
+    [SerializeField]
+    public EnemyStats enemy;
+
+    public Transform player;
+    public int chaseDistance;
+    public int findDistance;
+
+
+    UnityEngine.AI.NavMeshAgent agent;
+    public GameObject[] points;
+    public int destPoint = 0;
+
+    public enum Behaviours { Patrol, Combat, Dead };
+    public Behaviours currBehaviour = Behaviours.Patrol;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        points = GameObject.FindGameObjectsWithTag("Waypoint");
+    }
+
+    void Update()
+    {
+        RunBehaviours();
+
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isDead", isDead);
+
+        if (enemy.Health <= 0)
+        {
+            isDead = true;
+            RunDeadState();
+        }
+        else
+        {
+            isDead = false;
+        }
+    }
+
+    void RunBehaviours()
+    {
+        switch (currBehaviour)
+        {
+            case Behaviours.Patrol:
+                RunPatrolState();
+                break;
+            case Behaviours.Combat:
+                RunCombatState();
+                break;
+            case Behaviours.Dead:
+                break;
+        }
+    }
+
+    void RunDeadState()
+    {
+        agent.SetDestination(this.transform.position);
+        currBehaviour = Behaviours.Dead;
+        isMoving = false;
+    }
+
+    private void RunCombatState()
+    {
+        isMoving = true;
+        if (Vector3.Distance(transform.position, player.position) > chaseDistance)
+        {
+            currBehaviour = Behaviours.Patrol;
+        }
+        else
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+
+    private void RunPatrolState()
+    {
+        isMoving = true;
+        if (Vector3.Distance(transform.position, player.position) < findDistance)
+        {
+            currBehaviour = Behaviours.Combat;
+        }
+        else if (agent.remainingDistance < 0.5f)
+        {
+            if (points.Length == 0)
+            {
+                return;
+            }
+            agent.SetDestination(points[destPoint].transform.position);
+            //if goes higher than the total number of waypoints -> go back to start of array
+            destPoint = (destPoint + 1) % points.Length;
+        }
+    }
+}
