fe8770815caf204a6f09757e96c1315ca325cd7b
James Home <jamesfoy17>
2019-03-16 21:04:39 +0000

    Updated turret Behaviour
    turrent now only rotates when the zombie gets within a certain range

--- Assets/TurretBehaviour.cs
+++ Assets/TurretBehaviour.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class TurretBehaviour : MonoBehaviour {
 
-    public Transform target;
+    [SerializeField]
+    ParticleSystem firingParticle;
+
     float strength = 1f;
     private Quaternion targetRotation;
     private float str;
-	
-	// Update is called once per frame
-	void LateUpdate ()
+    private Transform target;
+    private bool isAiming = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        //This line finds the rotation from this object to the target
-        targetRotation = Quaternion.LookRotation(target.position - transform.position);
-        //This line finds out how much you are wanting to lerp the rotation and prevents it from overshooting the rotation
-        str = Mathf.Min(strength * Time.deltaTime, 1);
-        //This line simply lerps the rotation of the object
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, str);
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Aiming");
+            target = other.gameObject.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if (isAiming == true)
+        {
+            firingParticle.Play();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Aim();
+    }
+
+    void Aim ()
+    {
+        if (target != null)
+        {
+            isAiming = true;
+            //This line finds the rotation from this object to the target
+            targetRotation = Quaternion.LookRotation(target.position - transform.position);
+            //This line finds out how much you are wanting to lerp the rotation and prevents it from overshooting the rotation
+            str = Mathf.Min(strength * Time.deltaTime, 1);
+            //This line simply lerps the rotation of the object
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, str);
+        }
+        else
+        {
+            isAiming = false;
+        }
 	}
 }
