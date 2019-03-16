91e1a354c622628f2eaac191b7b44da313e7e979
James Home <jamesfoy17>
2019-03-16 12:08:05 +0000

    Added a turret script
    the turret now rotates to look at a target

---
+++ Assets/TurretBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBehaviour : MonoBehaviour {
+
+    public Transform target;
+    float strength = 1f;
+    private Quaternion targetRotation;
+    private float str;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        //This line finds the rotation from this object to the target
+        targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        //This line finds out how much you are wanting to lerp the rotation and prevents it from overshooting the rotation
+        str = Mathf.Min(strength * Time.deltaTime, 1);
+        //This line simply lerps the rotation of the object
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, str);
+	}
+}
