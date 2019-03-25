a382b9bcf7cd04d5350cf29fe72d0135fb864d2d
James Home <jamesfoy17>
2019-03-25 11:25:40 +0000

    Added Basic Grenade Script

---
+++ Assets/Scripts/Items/Grenade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+
+    [SerializeField]
+    float delay;
+
+    [SerializeField]
+    GameObject explosionEffect;
+
+    float countDown;
+
+    bool hasExploded = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        countDown = delay;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+
+        if (countDown <= 0 && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+	}
+
+    private void Explode()
+    {
+        //Show Explosion Effect;
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        //Get nearby objects and add force and destory/kill
+
+        Destroy(gameObject);
+    }
+}
