8c99ea19bfd82b2dd07aedee91c5906e580b1e48
Jfoy <jamesfoy17@gmail.com>
2019-04-03 11:21:02 +0100

    Added New Models

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Base class for all enemies
+public class Enemy : MonoBehaviour
+{
+    public int health;
+
+    public int speed;
+
+    public int damage;
+
+    [SerializeField]
+    PlayerStats player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
